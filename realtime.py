0c3eaef0c525cd3fcd97bf426d6a9de6f04e2ae6
Joong Sim <joonjooly@gmail.com>
2021-04-09 12:54:20 -0700

    changed cutoff time calculation to use runtime

--- realtime.py
+++ realtime.py
@@ -4,6 +4,7 @@ import sys
 from datetime import datetime
 from dateutil import parser
 from math import floor
+from time import sleep
 
 
 def _epochtime(row):
@@ -39,56 +40,74 @@ def get_past_data(df, cutoff):
     return df, past_data
 
 
+def print_info(time, sample):
+    print('Time: ' + str(time))
+    print('Sample:\n')
+    print(sample)
 
-def realtime(data, speed, refresh):
+def realtime(data, speed, refresh, func):
     
     '''
       CURRENT:
     ======================================================================
-    instead of using actual runtime, can use simulated runtime
-    simulated runtime = refresh time * speed * iteration
+    separates dataframe based on epoch
+    cutoff based on runtime of program 
+    sleep time is modified to ensure accuracy < 20 ms (roughly)
     
-    start time is set to epoch time of first row, rounded down
-
+    
 
           OBSOLETE:
         ======================================================================
-        separates dataframe based on epoch
-        cutoff based on runtime of program 
-        Changed to current implementation due to time.sleep() inconsistencies
+        instead of using actual runtime, can use simulated runtime
+        simulated runtime = refresh time * speed * iteration
+        
+        start time is set to epoch time of first row, rounded down
     '''
 
     earliest = floor(data.iloc[0].epoch)
     start = datetime.now().timestamp()
-    i = 0.0
+    
+    #i = 0.0
 
     while len(data) > 0:
-        cutoff = (refresh * speed * i) + earliest
+        inner_start = datetime.now().timestamp()
+        
+        print('Total time: ' + str(runtime(start)))
+
+        cutoff_time = runtime(start) * speed + earliest
+        data, sample = get_past_data(data, cutoff_time)
+        
+        
+        
+        '''time_elapsed = refresh * speed * i
+        cutoff = time_elapsed + earliest
         data, sample = get_past_data(data, cutoff)
+        '''
+        func(cutoff_time, sample)
         
-        print('Time: ' + str(refresh * speed * i))
-        print('Test: ' + str(len(data)))
-        print('Sample:\n')
-        print(sample)
         
-        i += 1
-        
-        input('Continue?\n')
-        #time.sleep(refresh)
+        '''
+        #whatever logic you want executed per cycle goes here
+        #ex: plot on map
+        #maybe in future add function as a parameter
+        '''
+        inner_runtime = runtime(inner_start)
+        print('r - i: ' + str(refresh-inner_runtime))    
+        sleep(refresh - inner_runtime - 0.01)
 
 
 if __name__ == '__main__':
     
     
     path = sys.argv[1]
-    speed = int(sys.argv[2])
-    refresh = int(sys.argv[3])
+    speed = float(sys.argv[2])
+    refresh = float(sys.argv[3])
     
 
     df = load_data(path)    
     input('Data loaded. Press any key to continue.\n')
     
-    realtime(df, speed, refresh)
+    realtime(df, speed, refresh, print_info)
     
 
     
